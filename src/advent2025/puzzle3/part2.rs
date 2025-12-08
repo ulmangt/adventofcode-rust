419eb52de7e05a0df33705242bf26247c2097c31
Geoffrey Ulman <geoff.ulman@gmail.com>
2025-12-07 23:18:24 -0500

    Incorrect greedy solution to 2025 puzzle 3 part 2

--- src/advent2025/puzzle3/part2.rs
+++ src/advent2025/puzzle3/part2.rs
@@ -1,5 +1,3 @@
-use itertools::Itertools;
-
 use crate::advent2025::puzzle3::part1::{InputDataError, parse_input_data, read_input_data};
 
 
@@ -8,25 +6,46 @@ pub fn solve( ) -> Result<u64,InputDataError> {
 
     Ok(banks.iter()
          .map(|bank| get_max_joltage(&bank,12))
+         .map(|max|{
+            println!( "got max {max}");
+            max
+         })
          .sum())
 }
 
 
-fn get_max_joltage( bank:&[u64], num_values: usize ) -> u64 {
+fn get_max_joltage( bank:&[u64], max_enabled: u32 ) -> u64 {
 
-    let all_combinations: Vec<Vec<&u64>> = bank.iter().combinations(num_values).collect();
+    let mut enabled_indexes = vec![false;bank.len()];
 
-    let max = all_combinations
-        .iter()
-        .map(|combination|get_joltage(combination))
-        .max()
-        .unwrap_or(0);
+    let mut enabled_count = 0;
+    for digit in (0..=9).rev() {
+        for index in 0..bank.len() {
+            if !enabled_indexes[index] && bank[index] == digit {
+                enabled_indexes[index] = true;
+                enabled_count = enabled_count+1;
+            }
 
-    max
+            if enabled_count == max_enabled {
+                return get_joltage(&bank, &enabled_indexes, max_enabled);
+            }
+        }
+    }
+
+    return get_joltage(&bank, &enabled_indexes, max_enabled);
 }
 
-fn get_joltage( bank:&[&u64] ) -> u64 {
-    (0..bank.len())
-        .map(|index| bank[index]*(10 as u64).pow(bank.len() as u32-index as u32 -1))
-        .sum()
+fn get_joltage( bank:&[u64], enabled_indexes: &[bool], max_enabled: u32 ) -> u64 {
+
+    let mut joltage: u64 = 0;
+    let mut remaining_enabled: u32 = max_enabled;
+
+    for index in 0..bank.len() {
+        if enabled_indexes[index] {
+            joltage = joltage + bank[index] * (10 as u64).pow(remaining_enabled-1);
+            remaining_enabled = remaining_enabled-1;
+        }
+    }
+
+    joltage
 }
