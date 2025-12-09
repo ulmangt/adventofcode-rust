f7c0d38aa342d589c4bc003820ce46356e3efc2b
Geoffrey Ulman <geoff.ulman@gmail.com>
2025-12-09 00:33:01 -0500

    Correct 2025 puzzle 7 part 2

--- src/advent2025/puzzle7/part2.rs
+++ src/advent2025/puzzle7/part2.rs
@@ -1,6 +1,6 @@
 use crate::{advent2024::puzzle4::part1::Matrix, advent2025::{puzzle4::part1::parse_input_data, puzzle7::part1::{InputDataError, read_input_data, step}}, main};
 
-pub fn solve() -> Result<usize,InputDataError> {
+pub fn solve() -> Result<u64,InputDataError> {
 
     let mut manifold: Matrix<char> = parse_input_data( read_input_data()?);
 
@@ -9,7 +9,29 @@ pub fn solve() -> Result<usize,InputDataError> {
         //println!("\n{}", manifold.to_string());
     }
 
-    Ok(count_paths(&manifold, 1, get_first_pipe_column( &manifold, 1).unwrap()))
+    let mut path_counter: Matrix<u64> = new_matrix( 1, manifold.rows(), manifold.cols());
+
+    for row in (0..manifold.rows()).rev() {
+        for col in 0..manifold.cols() {
+            let value = manifold.get_unsigned( row, col );
+
+            match value {
+                Some('|') => {
+                    path_counter.set_unsigned(row-1, col, *path_counter.get_unsigned(row, col).unwrap());
+                },
+                Some('^') => {
+                    path_counter.set_unsigned(row-1, col, *path_counter.get_unsigned(row, col-1).unwrap() + *path_counter.get_unsigned(row, col+1).unwrap());
+                }
+                _ => {
+                    // do nothing
+                }
+            }
+        }
+    }
+
+    println!("\n{}", path_counter.to_string());
+
+    Ok(*path_counter.get_unsigned(0,get_first_pipe_column( &manifold, 1).unwrap()).unwrap())
 }
 
 pub fn count_paths( manifold: &Matrix<char>, row: usize, col: usize ) -> usize {
@@ -37,4 +59,18 @@ fn get_first_pipe_column( manifold: &Matrix<char>, row: usize ) -> Option<usize>
     }
 
     return None;
+}
+
+fn new_matrix( default_value: u64, rows: usize, cols: usize ) -> Matrix<u64> {
+    let mut data = Vec::new();
+
+    for _ in 0..rows {
+        let mut col_data = Vec::new();
+        for _ in 0..cols {
+            col_data.push(default_value);
+        }
+        data.push(col_data);
+    }
+
+    Matrix::new(data)
 }
